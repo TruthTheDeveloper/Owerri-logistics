834132242e165ad15a03977c1884ba56a941525a
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-11 14:47:42 -0800

    11th march 2022

--- components/HomePage/ContactUs.tsx
+++ components/HomePage/ContactUs.tsx
@@ -4,16 +4,18 @@ import type { NextPage } from 'next';
 
 const ContactUs: NextPage = () => {
   return (
-      <div>
-          <h1>Contact Us</h1>
-          <div>
-              <input />
-          </div>
-          <div>
-              <input />
-          </div>
-          <div>
-              <textarea />
+      <div className="">
+          <h1 className=" font-medium text-center text-2xl mb-6">Contact Us</h1>
+          <div className="flex flex-col ml bg-black">
+            <div className="w-80 mx-auto my-2 ">
+                <input className=" py-1 w-full "  />
+            </div>
+            <div className="w-80 mx-auto my-2">
+                <input className="px-16 py-1 w-full"  />
+            </div>
+            <div className="w-80 mx-auto my-2">
+                <textarea className="px-16 py-1 w-full h-28"  />
+            </div>
           </div>
       </div>
   )
