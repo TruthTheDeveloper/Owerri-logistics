3aac55d06e61ebf0335ebb8b7c711691374e9007
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-11 15:08:52 -0800

    11th march 2022

--- components/HomePage/ContactUs.tsx
+++ components/HomePage/ContactUs.tsx
@@ -13,10 +13,14 @@ const ContactUs: NextPage = () => {
             <div className="w-80 mx-auto my-3">
                 <input className=" py-1 w-full bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="EMAIL" />
             </div>
-            <div className="w-80 mx-auto my-3">
+            <div className="w-80 mx-auto my-3 ">
                 <textarea className=" py-1 w-full h-28 bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="MESSAGE" />
             </div>
+            <div className="mx-auto">
+            <button className="bg-green-400 px-6 py-2 text-white rounded-md ">Contact Us</button>
           </div>
+          </div>
+          
       </div>
   )
 }
