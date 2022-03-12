7842d8af401c77df79a1e4921e264599cfea1b89
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-11 16:20:17 -0800

    11th march 2022

--- components/HomePage/Header.tsx
+++ components/HomePage/Header.tsx
@@ -5,15 +5,15 @@ import bike from '../../public/assets/bike.svg'
 
 const Header: NextPage = () => {
   return (
-      <div className="md:flex">
-          <div className="text-3xl text-center md:text-left md:pt-4 mt-5 md:basis1/2 ">
+      <div className="md:flex md:mx-12 lg:mx-28 xl:mx-32">
+          <div className=" text-3xl xl:text-4xl text-center md:text-left md:pt-4 mt-5 md:basis-1/2  ">
               <p><span className="text-green-500">Get</span> your Delivery</p>
               <p>at your doorstep anywhere</p>
               <p>around <span className="text-green-500">Owerri</span> by the best</p>
               <p>dispatch rider</p>
           </div>
-          <div className=" md:mt-5 md:basis1/2 ml-16 sm:ml-28 ">
-                <Image src={bike}  alt="text" width={400} height={300}/>
+          <div className=" md:mt-5 md:basis-1/2  ml-10">
+                <Image src={bike}  alt="text" width={500} height={300}/>
           </div>
       </div>
   )
--- components/HomePage/Navbar.tsx
+++ components/HomePage/Navbar.tsx
@@ -7,6 +7,7 @@ const Navbar: NextPage = () => {
           <h1 className="text-4xl font-semibold mt-2 mx-2"><span className="text-green-500">Owerri</span> Logistics</h1>
           <ul className="lg:flex lg:mr-24 lg:mt-2 hidden">
               <li className="lg:text-lg lg:mx-4"><a href="#">Home</a></li>
+              <li className="lg:text-lg lg:mx-4"><a href="#">Track Item</a></li>
               <li className="lg:text-lg lg:mx-4"><a href="#">Contact Us</a></li>
               <li className="lg:text-lg lg:mx-4"><a href="#">About Us</a></li>
           </ul>
