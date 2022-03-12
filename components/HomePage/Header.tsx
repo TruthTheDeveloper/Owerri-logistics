291f4ef695ad6b29d46732106f3c71065d0506bc
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-11 16:03:27 -0800

    11th march 2022

--- components/HomePage/Header.tsx
+++ components/HomePage/Header.tsx
@@ -6,14 +6,14 @@ import bike from '../../public/assets/bike.svg'
 const Header: NextPage = () => {
   return (
       <div className="md:flex">
-          <div className="text-3xl text-center mt-5 md:basis1/2 bg-orange-300">
+          <div className="text-3xl text-center md:text-left md:pt-4 mt-5 md:basis1/2 ">
               <p><span className="text-green-500">Get</span> your Delivery</p>
               <p>at your doorstep anywhere</p>
               <p>around <span className="text-green-500">Owerri</span> by the best</p>
               <p>dispatch rider</p>
           </div>
-          <div className="mt-14 md:basis1/2 bg-red-600 ">
-              <Image src={bike}  alt="text" width={500} height={500}/>
+          <div className=" md:mt-5 md:basis1/2 ml-16 sm:ml-28 ">
+                <Image src={bike}  alt="text" width={400} height={300}/>
           </div>
       </div>
   )
