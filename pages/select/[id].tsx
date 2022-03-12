497d3cfbab40beb058b9dced0429e31595f42a66
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-12 13:22:04 -0800

    12 march 2022

--- pages/select/[id].tsx
+++ pages/select/[id].tsx
@@ -2,6 +2,9 @@ import type { NextPage } from 'next';
 import Layout from '../../components/Layout/Layout';
 import Image from 'next/image';
 import map from '../../public/assets/map.png'
+import { faDirections } from '@fortawesome/free-solid-svg-icons';
+import { faLocation } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 
 
@@ -16,36 +19,42 @@ const Select: NextPage = () => {
                 <p>Owerri logistics eliminates the nedd to leave your home or comfort zone, trying to dispatch items</p>
             </div>
 
-            <div className="bg-white mx-8 my-8 shadow-lg pt-16">
-                <div className="flex px-8">
+            <div className="bg-white mx-8 sm:mx-16 my-8 shadow-lg pt-16">
+                <div className="flex px-8 sm:px-24">
                     <div className="basis-1/3">
                         <div className="border-t-4 border-black">
-                            <div style={{marginTop:'-1rem'}} className=" mx-12 py-1 px-3   rounded-full bg-black text-white text-center text-sm">1</div>
+                            <div style={{marginTop:'-1rem'}} className=" mx-auto py-1 w-8   rounded-full bg-black text-white text-center text-sm">1</div>
                         </div>
                         <h1 className="text-sm text-center my-4">Pick up location</h1>
                     </div>
                     <div className="basis-1/3 border-t-4 border-slate-200">
-                        <div style={{marginTop:'-1rem'}} className=" mx-12 py-1 px-3   z-10 rounded-full bg-white border-slate-200 border text-slate-400  text-center text-sm">2</div>
+                        <div style={{marginTop:'-1rem'}} className=" mx-auto py-1 w-8   z-10 rounded-full bg-white border-slate-200 border text-slate-400  text-center text-sm">2</div>
                         <h1 className="text-sm text-center my-4">Shipment details</h1>
                     </div>
                     <div className="basis-1/3 border-t-4 border-slate-200">
-                        <div style={{marginTop:'-1rem'}} className=" mx-12 py-1 px-3    z-10 rounded-full bg-white border-slate-200 border text-slate-400   text-center text-sm">
+                        <div style={{marginTop:'-1rem'}} className=" mx-auto py-1 w-8    z-10 rounded-full bg-white border-slate-200 border text-slate-400   text-center text-sm">
                             3
                         </div>
                         <h1 className="text-sm text-center my-4">Upload item</h1>
                     </div>
                 </div>
-                <div className="flex flex-col mx-12">
-                    <div className="flex flex-col py-2">
-                        <label className="mb-2">Pickup Address</label>
-                        <input className="h-10 p-4 placeholder:text-sm placeholder:pl-2 outline-none bg-slate-100 rounded-md" placeholder="Pickup Address"/>
+                <div className="flex flex-col md:flex-row md:justify-between  mx-6 sm:mx-16">
+                    <div className="flex flex-col  py-2">
+                        <label className="mb-2 ml-8">Pickup Address</label>
+                        <div className="flex" >
+                            <span className="mx-2"><FontAwesomeIcon icon={faDirections}  /></span>
+                            <input className="h-10 p-4 placeholder:text-sm w-full placeholder:pl-2 outline-none bg-slate-100 rounded-md" placeholder="Pickup Address"/>
+                        </div>
                     </div>
                     <div className="flex flex-col py-2">
-                        <label className="mb-2">Delivery Address</label>
-                        <input className="h-10 p-4 placeholder:text-sm placeholder:pl-2 outline-none mb-4 bg-slate-100 rounded-md" placeholder="Delivery Address"/>
+                        <label className="mb-2 ml-8">Delivery Address</label>
+                        <div className="flex">
+                            <span className="mx-2"><FontAwesomeIcon icon={faLocation}  /></span>
+                            <input className="h-10 p-4 w-full placeholder:text-sm placeholder:pl-2 outline-none mb-4 bg-slate-100 rounded-md" placeholder="Delivery Address"/>
+                        </div>
                     </div>
                 </div>
-                    <div className="m-4">
+                    <div className="m-4 sm:mx-8">
                         <Image
                         src={map}
                         alt="text"
