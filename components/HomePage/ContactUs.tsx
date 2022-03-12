7942fafc8451d6f44e4cd84f3cfc3cf6974b958d
TruthTheDeveloper <henrysempire111gmail.com>
2022-03-11 20:44:24 -0800

    11th march 2022

--- components/HomePage/ContactUs.tsx
+++ components/HomePage/ContactUs.tsx
@@ -7,14 +7,14 @@ const ContactUs: NextPage = () => {
       <div className="">
           <h1 className=" font-medium text-center text-2xl my-6">Contact Us</h1>
           <div className="flex flex-col mb-10">
-            <div className="w-full my-3 ">
-                <input className=" py-2 bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="FULL NAME" />
+            <div className="w-full flex justify-center my-3">
+                <input className=" py-2 w-9/12 md:w-7/12 lg:w-6/12 xl:w-5/12   bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="FULL NAME" />
             </div>
-            <div className="w-full my-3">
-                <input className="py-2 bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="EMAIL" />
+            <div className="w-full flex justify-center my-3">
+                <input className="py-2 w-9/12 md:w-7/12 lg:w-6/12 xl:w-5/12  bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="EMAIL" />
             </div>
-            <div className="w-full my-3 ">
-                <textarea className="py-2  h-44 bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="MESSAGE" />
+            <div className="w-full flex justify-center my-3 ">
+                <textarea className="py-2 w-9/12 md:w-7/12 lg:w-6/12 xl:w-5/12  h-44 bg-slate-100 placeholder:text-sm pl-2 outline-none" placeholder="MESSAGE" />
             </div>
             <div className="mx-auto">
             <button className="bg-green-400 px-6 py-2 text-white rounded-md ">Contact Us</button>
