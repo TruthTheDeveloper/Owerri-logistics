15e210caaa4e4f965545c7d252292a8b12835ee5
TruthTheDeveloper <henrytruthidongesit@gmail.com>
2022-03-14 02:19:16 -0700

    first commit

--- components/HomePage/ChooseDeliveryMethod.tsx
+++ components/HomePage/ChooseDeliveryMethod.tsx
@@ -6,32 +6,84 @@ interface Props{
     vechicle:any;
     vechicleName:string;
     link:string;
+    description:string;
 }
 
-const ChooseDeliveryMethod:NextPage<Props> = ({vechicle, vechicleName, link}) => {
+const ChooseDeliveryMethod:NextPage<Props> = ({vechicle, vechicleName, link, description}) => {
 
     let deliverMethodBox = null
     if(link === "motorbike"){
-        
+        deliverMethodBox = <Link href="/select/motorbike">
+        <div className="justify-self-center border-2 hover:border-green-400 cursor-pointer  w-48 h-52 mb-12 shadow-lg rounded-md ">
+            <div className="mx-16 mt-4 mb-4">
+                <Image
+                    src={vechicle}
+                    alt="text"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <div className="text-center text-sm">
+                <h1 className="font-medium text-xl mb-2">{vechicleName}</h1>
+                    <p>{description}</p>
+                </div>
+            </div>
+      </Link>
+    }else if(link === "car"){
+        deliverMethodBox = <Link href="/select/car">
+        <div className="justify-self-center border-2 hover:border-green-400 cursor-pointer  w-48 h-52 mb-12 shadow-lg rounded-md ">
+            <div className="mx-16 mt-4 mb-4">
+                <Image
+                    src={vechicle}
+                    alt="text"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <div className="text-center text-sm">
+                <h1 className="font-medium text-xl mb-2">{vechicleName}</h1>
+                    <p>{description}</p>
+                </div>
+            </div>
+      </Link>
+    }else if(link == 'van'){
+        deliverMethodBox = <Link href="/select/van">
+        <div className="justify-self-center border-2 hover:border-green-400 cursor-pointer  w-48 h-52 mb-12 shadow-lg rounded-md ">
+            <div className="mx-16 mt-4 mb-4">
+                <Image
+                    src={vechicle}
+                    alt="text"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <div className="text-center text-sm">
+                <h1 className="font-medium text-xl mb-2">{vechicleName}</h1>
+                    <p>{description}</p>
+                </div>
+            </div>
+      </Link>
+    }else if(link === 'truck'){
+        deliverMethodBox = <Link href="/select/truck">
+        <div className="justify-self-center border-2 hover:border-green-400 cursor-pointer  w-48 h-52 mb-12 shadow-lg rounded-md ">
+            <div className="mx-16 mt-4 mb-4">
+                <Image
+                    src={vechicle}
+                    alt="text"
+                    width={50}
+                    height={50}
+                />
+            </div>
+            <div className="text-center text-sm">
+                <h1 className="font-medium text-xl mb-2">{vechicleName}</h1>
+                    <p>{description}</p>
+                </div>
+            </div>
+      </Link>
     }
 
     return (
-        <Link href="/select/motorbike">
-            <div className="justify-self-center border-2 hover:border-green-400 cursor-pointer  w-48 h-52 mb-12 shadow-lg rounded-md ">
-                <div className="mx-16 mt-4 mb-4">
-                    <Image
-                        src={vechicle}
-                        alt="text"
-                        width={50}
-                        height={50}
-                    />
-                </div>
-                <div className="text-center text-sm">
-                    <h1 className="font-medium text-xl mb-2">{vechicleName}</h1>
-                        <p>Ideal for lightweight items Documents, human hair, phones</p>
-                    </div>
-                </div>
-          </Link>
+        <>{deliverMethodBox}</>
     )
 }
 
--- components/HomePage/Main.tsx
+++ components/HomePage/Main.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import ChooseDeliveryMethod from './ChooseDeliveryMethod';
 import car from '../../public/assets/car 1.png';
-import delivery from '../../public/assets/delivery-van 1.png';
+import truck from '../../public/assets/delivery-van 1.png';
 import van from '../../public/assets/van 1.png';
 import motorbike from '../../public/assets/motorbike 1.png';
 
@@ -16,10 +16,11 @@ const Main: NextPage = () => {
           </div>
         <div className="grid grid-rows-5 md:grid-rows-2 md:grid-cols-2 bg-white  mx-8 md:mx-16 lg:mx-32 xl:mx-56 2xl:mx-64 mb-12">
 
-        <ChooseDeliveryMethod vechicle={motorbike} vechicleName={"Bike"} link="motorbike"/>
-          <ChooseDeliveryMethod vechicle={car} vechicleName={"Car"} link="car"/>
-          <ChooseDeliveryMethod vechicle={van} vechicleName={"Van"} link="van"/>
-          <ChooseDeliveryMethod vechicle={truck} vechicleName={"Truck"} link="truck"/>
+        <ChooseDeliveryMethod vechicle={motorbike} vechicleName="Bike" link="motorbike" description="Ideal for lightweight items Documents, human hair, phones"/>
+          <ChooseDeliveryMethod vechicle={car} vechicleName="Car" link="car" description="For items that will fit into a car boot:Bag of clothes and shoes a microwave"/>
+          <ChooseDeliveryMethod vechicle={van} vechicleName="Van" link="van" description="For items too large to fit in a car boot: Large Bag of clothes or shoes, a small fridge e.t.c "/>
+          <ChooseDeliveryMethod vechicle={truck} vechicleName="Truck" link="truck" description="For items that will fit into a car boot:Bag of clothes and shoes a microwave"/>
+        </div>
       </div>
   )
 }
